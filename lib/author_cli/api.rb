14857ae9be8da76de59817af2704e25d97cea94d
Cailie.Skelton <CSkelton707@gmail.com>
2020-08-24 22:45:01 -0400

    Fixed gemspec

--- lib/author_cli/api.rb
+++ lib/author_cli/api.rb
@@ -7,18 +7,19 @@ require net/http
 require open-uri 
 
 class Authorcli::Api
-    attr_accessor :title
+ attr_accessor :title
     def initialize(title)
         @title = title
         @key = [QPGP53kmsToQQAzsQfnhDKqqZFGbgg50]
-        @URL = [https://api.nytimes.com/svc/books/v3/reviews.json
-]
+        @URL = [https://api.nytimes.com/svc/books/v3/reviews.json?title=#{title}]
     end
 
     def fetch_data
-        
         url = "https://api.nytimes.com/svc/books/v3/reviews.json"
-        response = HTTParty.get(url)
+      
         response.parsed_response #may be unnecessary
+        title = response["main"]["review"]
+        review = response["title"]
+        turn into objects, fill out information related to the title/review information
     end
 end
