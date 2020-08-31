814c5b86d295936551b93f5256e1371e16c2ba49
CailieS <63619967+CailieS@users.noreply.github.com>
2020-08-31 12:10:00 -0400

    Update cli.rb

--- lib/author_cli/cli.rb
+++ lib/author_cli/cli.rb
@@ -2,43 +2,41 @@ require_relative "./version.rb"
 require 'pry'
 class AuthorCli::Cli 
    
-    def call
-        puts "Hello, and welcome to the New York Times author Cli! Please enter an authors name and receive their book information."
-        puts "To exit the program, enter Exit."
-        menu
-     end
- 
+  def call
+    puts "Hello, and welcome to the New York Times author Cli! Please enter an authors name and receive their book information."
+    menu
+ end
 
-   def menu
-    input = gets.strip.downcase.split(" ") 
-    if AuthorCli::API.get_data(input[0], input[1]) != nil
-       book_list
-       menu
-    elsif input == "exit"
-        goodbye
-    else 
-      invalid_entry
-      menu
-    end
+ def menu
+  input = gets.strip.downcase.split(" ")
+  if AuthorCli::API.get_data(input[0], input[1]) != nil
+    book_list
+  elsif input == "exit"
+      goodbye
+  else 
+    invalid_entry
+    menu
   end
+end
 
-   def book_list
-     AuthorCli::Book.all.each_with_index do |book, index|
-        puts "#{index+1}.#{book.title}"
-      end
-     puts "Which book would you like a synopsis for?"
-     input = gets.strip.downcase
-     if input == 'exit'
-       goodbye
-     else
-       summary(AuthorCli::Book.all[input.to_i - 1].title)
-     end
+
+ def book_list
+   AuthorCli::Book.all.each_with_index do |book, index|
+     puts "#{index+1}.#{book.title}"
    end
+   puts "Which book would you like a synopsis for?"
+   input = gets.strip.downcase
+   if input == 'exit'
+     goodbye
+  else
+    summary(AuthorCli::Book.all[input.to_i - 1].title)
+  end
+ end
 
-    def summary(title)
-      puts AuthorCli::Book.find_by_name(title).summary
-      book_list
-    end
+     def summary(title)
+       puts AuthorCli::Book.find_by_name(title).summary
+       book_list
+     end
    
 
     def invalid_entry
@@ -50,5 +48,4 @@ class AuthorCli::Cli
         puts "See you later!"
     end
  
-#    #binding.pry
 end
